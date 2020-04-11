279d7ecad33bac932d3f1e03f4a9baba41a4410e
Yeasir H <yeasir01@gmail.com>
2020-04-11 01:15:17 -0700

    setup basic mysql schema & seeds

---
+++ db/schema.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burger_tbl (burger, devoured)
+VALUES ("Double Double", true);
+
+INSERT INTO burger_tbl (burger, devoured)
+VALUES ("Chicken Parm", false);
+
+INSERT INTO burger_tbl (burger, devoured)
+VALUES ("Steak Burger", false);
