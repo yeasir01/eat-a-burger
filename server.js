3a9def56b267cd016927b79ee7a1c2d204515f99
Yeasir H <yeasir01@gmail.com>
2020-04-12 21:40:50 -0700

    added CRUD routes

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.use(express.urlencoded({
-	extended: true
-}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.engine("handlebars", exphbs());
@@ -29,6 +27,39 @@ app.get("/api/burgers", function (req, res) {
 	});
 });
 
+app.post("/api/burgers", function (req, res) {
+	db.burgers_tbl.create({
+		burger: req.body.burger,
+		devoured: req.body.devoured
+	}).then(function (burgersDb) {
+		res.json(burgersDb);
+	});
+});
+
+app.put("/api/burgers", function (req, res) {
+	db.burgers_tbl.update({
+			devoured: req.body.devoured
+		}, {
+			where: {
+				id: req.body.id
+			}
+		}).then(function (burgersDb) {
+			res.json(burgersDb);
+		})
+		.catch(function (err) {
+			res.json(err);
+		});
+});
+
+app.delete("/api/burgers/:id", function(req, res) {
+    db.burgers_tbl.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(burgersDb) {
+      res.json(burgersDb);
+    });
+});
 
 
 db.sequelize.sync().then(() => {
