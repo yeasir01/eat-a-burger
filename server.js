2ba627fe90cefd8fac511c77d18e71f6879b7549
Yeasir H <yeasir01@gmail.com>
2020-04-25 11:27:36 -0700

    correction

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(compression())
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
